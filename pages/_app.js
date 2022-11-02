6bd5b222fb4fca06dae0497eaa37a2873b6733cc
RJ Goetz <rj@rjgoetz.com>
2022-11-02 14:34:54 -0500

    google ga4 code

--- pages/_app.js
+++ pages/_app.js
@@ -12,12 +12,13 @@ function MyApp({ Component, pageProps }) {
       </Head>
 
       <Component {...pageProps} />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-PG0NQC3BBN"></Script>
       <Script id="google-tag-manager" strategy="afterInteractive">
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-53ZJRP5');`}
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PG0NQC3BBN');`}
       </Script>
     </div>
   );
